92143f30eff1aff3aafe2fa00e72cd9f1ddac9f5
deveaston06 <deveaston06@gmail.com>
2025-08-12 19:36:39 +0800

    add rm deb file

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 npm install -g @mermaid-js/mermaid-cli
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
+rm ./google-chrome-stable_current_amd64.deb
